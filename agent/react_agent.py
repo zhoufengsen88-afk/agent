6e2c7386412a051f5f01a79a77f4461a25313894
周逢森 <zhoufengsen@az028.cn>
2026-05-14 19:40:42 +0800

    修改ai输出以及论文

--- agent/react_agent.py
+++ agent/react_agent.py
@@ -2,6 +2,7 @@ from langchain.agents import create_agent
 from agent.tools.middleware import monitor_tool, log_before_model, report_prompt_switch
 from agent.tools.agent_tools import (rag_summarize, get_weather, get_user_location, get_user_id, get_current_month,
                                      fetch_external_data, fill_context_for_report)
+from langchain_core.messages import AIMessage
 from model.factory import chat_model
 from utils.prompt_loader import load_system_prompt
 
@@ -29,7 +30,7 @@ class ReactAgent:
         }
         for chunk in self.agent.stream(input_dict, stream_mode="values", context=context):
             latest_message = chunk["messages"][-1]  # 有历史记录所以取最后一条
-            if latest_message.content:
+            if isinstance(latest_message, AIMessage) and latest_message.content:
                 yield latest_message.content.strip() + "\n"
 
 if __name__ == '__main__':
